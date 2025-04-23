d9cce276d6cc3bd7bff23149f32fe55eb815227d
adu014 <tiden86798@gmail.com>
2025-04-23 00:54:37 -0400

    Updates

---
+++ precache-manifest.9461bf7b1abdf8cbe06c1cfbce69e6c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e0f86815bc19c20a5e0b96de529280",
+    "url": "/Echo-Calculator/index.html"
+  },
+  {
+    "revision": "2e32441c69984a240a55",
+    "url": "/Echo-Calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c4c919bf9ceba85f310",
+    "url": "/Echo-Calculator/static/js/2.066143b6.chunk.js"
+  },
+  {
+    "revision": "b5a131c4f4a45d05103d19d28308490f",
+    "url": "/Echo-Calculator/static/js/2.066143b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be060e943709675026a1",
+    "url": "/Echo-Calculator/static/js/3.64a6ecb5.chunk.js"
+  },
+  {
+    "revision": "2e32441c69984a240a55",
+    "url": "/Echo-Calculator/static/js/main.47c34cad.chunk.js"
+  },
+  {
+    "revision": "e13a7093d1066f54b8c0",
+    "url": "/Echo-Calculator/static/js/runtime-main.f556872f.js"
+  }
+]);
